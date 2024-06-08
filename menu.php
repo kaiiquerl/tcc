b0439aa68a7daa132e99b715ce3abb2933edc30c
kaiiquerl <kaiiquerl21@gmail.com>
2024-06-08 19:04:07 -0300

    update

--- menu.php
+++ menu.php
@@ -22,11 +22,14 @@ include_once "conexao.php";
 <body>
     <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
         <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            BEM VINDO
+            Bem-vindo à SCHOOL STOCK
         </a>
 
+        <div class="submenu">
+            <img src="img/logo.svg" class="logoM" alt="logop">
+        </div>
+
         <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link">HOME</a></li>
             <li class="nav-item"><a href="#" class="nav-link">Sobre</a></li>
             <li class="nav-item"><a href="login.php" class="nav-link active" aria-current="page">Log Out</a></li>
         </ul>
@@ -35,9 +38,6 @@ include_once "conexao.php";
     <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
         </ul>
     </div>
-
-
-    <img src="img/logo.png" alt="logop" srcset="logop">
 
     <div class="container_menu">
         <ul class="list-unstyled ps-0">
@@ -185,7 +185,6 @@ include_once "conexao.php";
                 <div class="col-md-4 d-flex align-items-center">
                     <a class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
                         <svg class="bi" width="30" height="24">
-                            <!-- img footer -->
                             />
                         </svg>
                     </a>
