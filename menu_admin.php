005be2bb6a160e6cbe44b3df36ebf66e9ae23013
kaiiquerl <kaiiquerl21@gmail.com>
2024-06-26 17:39:17 -0300

    MENU

--- menu_admin.php
+++ menu_admin.php
@@ -5,25 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TI Inventario</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <link href="headers.css" rel="stylesheet">
-    <link href="heroes.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
-    <link href="sidebars.css" rel="stylesheet">
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body class="bg-light <?php echo $isAdmin ? 'admin-page' : 'user-page'; ?>">
-    <header class="bg-primary text-white py-3 <?php echo $isAdmin ? 'admin-header' : ''; ?>">
+<body class="bg-light">
+    <header class="bg-primary text-white py-3">
         <div class="container d-flex justify-content-between align-items-center">
             <img src="img/logo.svg" class="logoM" alt="logop">
-            <h1 class="h3">School Stock</h1>
+            <h1 class="h3 mb-0">School Stock</h1>
             <nav>
                 <ul class="nav">
                     <li class="nav-item"><a href="login.php" class="nav-link text-white">Log Out</a></li>
-                    <li class="nav-item"><a href="login.php" class="nav-link text-white">Sobre</a></li>
+                    <li class="nav-item"><a href="sobre.php" class="nav-link text-white">Sobre</a></li>
                 </ul>
             </nav>
         </div>
@@ -32,29 +26,27 @@
     <main class="container my-5">
         <div class="row">
             <aside class="col-md-3">
-                <div class="list-group">
+                <div class="list-group mb-3">
                     <a href="#" class="list-group-item list-group-item-action active">Bloco A</a>
-                    <div class="list-group">
-                        <a href="exibir_admin.php?bloco=A&lab=LAB 1" class="list-group-item list-group-item-action">Lab 1</a>
-                        <a href="exibir_admin.php?bloco=A&lab=LAB 2" class="list-group-item list-group-item-action">Lab 2</a>
-                        <a href="exibir_admin.php?bloco=A&lab=LAB 3" class="list-group-item list-group-item-action">Lab 3</a>
-                    </div><br>
-                    <a href="#" class="list-group-item list-group-item-action">Bloco B</a>
-                    <div class="list-group">
-                        <a href="exibir_admin.php?bloco=B&lab=LAB 1" class="list-group-item list-group-item-action">Lab 1</a>
-                        <a href="exibir_admin.php?bloco=B&lab=LAB 2" class="list-group-item list-group-item-action">Lab 2</a>
-                        <a href="exibir_admin.php?bloco=B&lab=LAB 3" class="list-group-item list-group-item-action">Lab 3</a>
-                    </div><br>
-                    <a href="#" class="list-group-item list-group-item-action">Bloco C</a>
-                    <div class="list-group">
-                        <a href="exibir_admin.php?bloco=C&lab=LAB 1" class="list-group-item list-group-item-action">Lab 1</a>
-                        <a href="exibir_admin.php?bloco=C&lab=LAB 2" class="list-group-item list-group-item-action">Lab 2</a>
-                        <a href="exibir_admin.php?bloco=C&lab=LAB 3" class="list-group-item list-group-item-action">Lab 3</a>
-                    </div>
+                    <a href="exibir_admin.php?bloco=A&lab=LAB 1" class="list-group-item list-group-item-action">Lab 1</a>
+                    <a href="exibir_admin.php?bloco=A&lab=LAB 2" class="list-group-item list-group-item-action">Lab 2</a>
+                    <a href="exibir_admin.php?bloco=A&lab=LAB 3" class="list-group-item list-group-item-action">Lab 3</a>
+                </div>
+                <div class="list-group mb-3">
+                    <a href="#" class="list-group-item list-group-item-action active">Bloco B</a>
+                    <a href="exibir_admin.php?bloco=B&lab=LAB 1" class="list-group-item list-group-item-action">Lab 1</a>
+                    <a href="exibir_admin.php?bloco=B&lab=LAB 2" class="list-group-item list-group-item-action">Lab 2</a>
+                    <a href="exibir_admin.php?bloco=B&lab=LAB 3" class="list-group-item list-group-item-action">Lab 3</a>
+                </div>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action active">Bloco C</a>
+                    <a href="exibir_admin.php?bloco=C&lab=LAB 1" class="list-group-item list-group-item-action">Lab 1</a>
+                    <a href="exibir_admin.php?bloco=C&lab=LAB 2" class="list-group-item list-group-item-action">Lab 2</a>
+                    <a href="exibir_admin.php?bloco=C&lab=LAB 3" class="list-group-item list-group-item-action">Lab 3</a>
                 </div>
             </aside>
             <section class="col-md-9">
-                <div class="p-5 bg-white rounded shadow-sm">
+                <div class="p-4 bg-white rounded shadow-sm">
                     <h2>Bem-vindos ao School Stock!</h2>
                     <p>Aqui, você pode registrar e gerenciar todos os patrimônios da sua escola de forma simples e eficiente. Estamos prontos para ajudar sua instituição a manter um inventário organizado. Explore nossa plataforma e simplifique sua gestão patrimonial conosco!</p>
                     <img src="img/sejabemvindo.svg" alt="Bem-vindo" class="img-fluid mt-3">
@@ -63,13 +55,13 @@
         </div>
     </main>
 
-    <footer class="bg-dark text-white py-4 mt-5 <?php echo $isAdmin ? 'admin-footer' : ''; ?>">
+    <footer class="bg-dark text-white py-4 mt-5">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-4 mb-3 mb-md-0">
                     <img src="img/logo_footer.svg" class="logof" alt="img footer">
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 mb-3 mb-md-0">
                     <h5>Contact</h5>
                     <p><img src="img/email.svg" class="logos" alt="img email"> Diretoria: e047dir@cps.sp.gov.br<br>Diretoria Administrativa: e047adm@cps.sp.gov.br<br>Diretoria Acadêmica: e047acad@cps.sp.gov.br</p>
                     <p><img src="img/telefone.svg" class="logos" alt="img telefone"> Tel.: (19) 3651-1229</p>
@@ -79,19 +71,11 @@
                     <p><img src="img/gps.svg" class="logos" alt="img gps"> Rodovia SP, 346 - km 204 - Morro Azul - CEP: 13990-000 - Espirito Santo do Pinhal/SP</p>
                 </div>
             </div>
-            <?php if ($isAdmin): ?>
-                <div class="text-center mt-3">
-                    <p>Você está logado como administrador.</p>
-                </div>
-            <?php endif; ?>
             <div class="text-center mt-3">
                 <p>&copy; Todos os Direitos Reservados...</p>
             </div>
         </div>
     </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
